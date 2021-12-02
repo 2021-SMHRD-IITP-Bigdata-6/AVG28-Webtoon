42b8ef0b99130df64c7c53efb5fb0242940f168f
smhrd <smhrd@DESKTOP-J185AOR>
2021-12-02 16:39:55 +0900

    222

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -8,13 +8,15 @@ public class Test {
 		//2. Tomcat 버전
 		//3. Encoding 방식(UTF-8)
 		//4. 너무피곤하네
-		System.out.println("First Code!!");
+		System.out.println("First Code!!!!!");
 		
 		System.out.println("GoHome");
 		
 		System.out.println("s");
 		System.out.println("다음사람");
 		System.out.println("다다음사람");
+		
+		System.out.println("!!E");
 	
 	}
 
